7ce639f866a82d3c8f175e5f6f37734d34665533
Matthew Leach <dev@mattleach.net>
2010-12-22 20:57:27 +0000

    Add the visualiser window class.

---
+++ src/visualiserWin.cpp
@@ -0,0 +1,110 @@
+/****************************************
+ *
+ * visualiserWin.cpp
+ * Defines a window class for visualisers.
+ *
+ * This file is part of mattulizer.
+ *
+ * Copyright 2010 (c) Matthew Leach.
+ *
+ * Mattulizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattulizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mattulizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "visualiserWin.h"
+#include "visualiser.h"
+#include "sdlexception.h"
+#include <SDL_timer.h>
+
+visualiserWin::visualiserWin(int desiredFrameRate,
+                             bool vsync,
+                             int width,
+                             int height,
+                             Uint32 flags)
+{
+	// Set the local members
+	this->desiredFrameRate = desiredFrameRate;
+	this->shouldVsync = vsync;
+	this->currentVis = NULL;
+	
+	// Set the OpenGL attributes
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	if(vsync)
+		SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 1);
+	
+	// Create the window
+	drawContext = SDL_SetVideoMode(width, height, 0, flags | SDL_OPENGL);
+	if(drawContext == NULL)
+		throw(SDLException());
+}
+
+void visualiserWin::setVisualiser(visualiser* vis)
+{
+	currentVis = vis;
+}
+
+void visualiserWin::registerEventHandler(visualiserEventHandler eh)
+{
+	eventHandlers.insert(eh);
+}
+
+void visualiserWin::eventLoop()
+{
+	bool wantToQuit = false;
+	SDL_Event* e;
+	while(!wantToQuit)
+	{
+		if(currentVis == NULL)
+		{
+			// wait for an event if we've not got a visualiser to show
+			SDL_WaitEvent(e);
+			if(e)
+				handleEvent(e);
+		}
+		else
+		{
+			// handle events...
+			SDL_PollEvent(e);
+			if(e)
+				handleEvent(e);
+			
+			// do some drawing
+			Uint32 before = SDL_GetTicks();
+			currentVis->draw();
+			Uint32 after = SDL_GetTicks();
+			
+			SDL_GL_SwapBuffers();
+			
+			if(!shouldVsync)
+			{
+				// Calculate the time taken to do the drawing
+				Uint32 timeTaken = after - before;
+				
+				// Delay to maintain the framerate
+				SDL_Delay(((Uint32)1000/desiredFrameRate) - timeTaken);
+			}
+		}
+	}
+}
+
+void visualiserWin::handleEvent(SDL_Event* e)
+{
+	for(std::set<visualiserEventHandler>::iterator i = eventHandlers.begin();
+	    i != eventHandlers.end(); i++)
+	{
+		// call the handler if the type is the same
+		if(i->type == e->type)
+			i->handler(e);
+	}
+}
---
+++ src/visualiserWin.h
@@ -0,0 +1,116 @@
+/****************************************
+ *
+ * visualiserWin.h
+ * Declares a window class for visualisers.
+ *
+ * This file is part of mattulizer.
+ *
+ * Copyright 2010 (c) Matthew Leach.
+ *
+ * Mattulizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattulizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mattulizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _VISUALISERWIN_H_
+#define _VISUALISERWIN_H_
+
+#include <SDL_video.h>
+#include <SDL_events.h>
+#include <set>
+class visualiser;
+
+/**
+ * Use this struct to register an event handler.
+ * Call the registerEventHandler() function with
+ * the type set to the type of event the callback
+ * should be called with and handler function pointer
+ * to a function that will handle the event.
+ */
+typedef struct
+{
+	Uint8 type;
+	void (*handler)(SDL_Event *e);
+}visualiserEventHandler;
+
+/**
+ * A type operator to compare two event handlers.
+ */
+template <typename visualiserEventHandler> struct handlerCMP
+{
+	bool operator() (const visualiserEventHandler& x,
+	                 const visualiserEventHandler& y) const
+	{
+		if(x.type == y.type)
+			if(x.handler == y.handler)
+				return true;
+		return false;
+	}
+};
+
+/**
+ * A visualiser window.
+ */
+class visualiserWin
+{
+	public:
+		/**
+		 * Initialise the visualiser, this is for internal use,
+		 * and should be called by the visualiser library only,
+		 * do not create a visualiser class yourself!
+		 * @param lib the mattulizer library instance class that contains
+		 * audio data.
+		 * @param desiredFrameRate the frame rate that will try and be achieved.
+		 * If you're using vsync, this parameter is ignored.
+		 * @param vsync weather to use vsync on the draw function.
+		 */
+		visualiserWin(int desiredFrameRate, 
+		              bool vsync,
+		              int width,
+		              int height,
+		              Uint32 flags);
+		
+		/**
+		 * Set the current visualiser. Until this is set,
+		 * the screen will be blank.
+		 * @param vis a reference to a visualiser object.
+		 */
+		void setVisualiser(visualiser* vis);
+		
+		/**
+		 * Register an event handler for a certian type of event.
+		 * @see visualiserEventHandler.
+		 * @param eH the event handler to register.
+		 */
+		void registerEventHandler(visualiserEventHandler eH);
+	private:
+		/**
+		 * This is the main event loop for the window.
+		 * It is implemented in the base class to abstract
+		 * away the SDL event loop control and SDL OpenGL setup
+		 * from the subclasses.
+		 */
+		void eventLoop();
+		/**
+		 * Called by the event loop to handle
+		 * all events.
+		 */
+		void handleEvent(SDL_Event* e);
+		SDL_Surface* drawContext;
+		int desiredFrameRate;
+		bool shouldVsync;
+		visualiser* currentVis;
+		std::set<visualiserEventHandler,
+		         handlerCMP<visualiserEventHandler> > eventHandlers;
+};
+
+#endif
