078810dcee1163b4355a1783f25d32ab9fa6369d
Matthew Leach <dev@mattleach.net>
2010-12-24 15:30:52 +0000

    Add docs to the dsp class.

--- src/dsp.h
+++ src/dsp.h
@@ -21,9 +21,28 @@
  * along with Mattulizer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * A pure abstract class for define a DSP plugin.
+ */
 class DSP
 {
 	public:
+		/**
+		 * This function is called to process the PCM data. It is called by the
+		 * DSP worker thread and should run as fast as possible. If work is still
+		 * happening when the DSPManager class receives the next batch of audio data
+		 * then that batch of samples is discarded. You can use the SEQ number to
+		 * detect this. It will be incremented every time the DSPManager
+		 * class attempts to process PCM data.
+		 * @param data the raw 16 bit signed PCM data.
+		 * @param len the number of samples in the buffer data.
+		 * @param SEQ the sequence number of this batch of PCM data.
+		 */
 		virtual void processPCMData(int16_t* data, int len, int SEQ) = 0;
+		
+		/**
+		 * This function is called by the visualiser class to get the PCM data.
+		 * @returns a void pointer to the processed PCM data.
+		 */
 		virtual void* getDSPData() = 0;
 };
