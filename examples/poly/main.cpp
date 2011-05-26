581d7d71d28034179217d3560c0d5b2360b09c08
Matthew Leach <dev@mattleach.net>
2011-05-26 21:13:32 +0100

    Add a poly visualiser.

---
+++ examples/poly/main.cpp
@@ -0,0 +1,91 @@
+/****************************************
+ *
+ * main.cpp
+ * poly program entry point.
+ *
+ * This file is part of mattulizer.
+ *
+ * Copyright 2011 (c) Matthew Leach.
+ *
+ * Mattulizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattulizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mattulizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../src/visualiserWin.h"
+#include "../../src/sdlexception.h"
+#include "poly.h"
+#include <iostream>
+#include <SDL.h>
+
+void usage(const char* fileName)
+{
+	std::cout << "Invalid number of arguments." << std::endl;
+	std::cout << fileName << " [audio file]" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	int sizex = 800;
+	int sizey = 600;
+	int fullscreen = 0;
+	int opt;
+	while((opt = getopt(argc, argv, "s:f")) != -1)
+	{
+		switch(opt)
+		{
+			case 's':
+				sizex = atoi(strtok(optarg, "x"));
+				sizey = atoi(strtok(NULL, "x"));
+				break;
+			case 'f':
+				fullscreen = SDL_FULLSCREEN;
+				break;
+			default:
+				usage(argv[0]);
+				return EXIT_FAILURE;
+		}
+	}
+	// at least one parameter expected.
+	if(optind >= argc)
+	{
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	// Initialise SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	// create a visualiser window.
+	visualiserWin win(0, true, sizex, sizey, fullscreen);
+	
+	// create an instance of the visualiser class.
+	poly polyVis(&win, 500, 0.003);
+	
+	// set the window's visualiser to the current one.
+	win.setVisualiser(&polyVis);
+	
+	// attempt to play the file.
+	try
+	{
+		std::string s(argv[optind]);
+		win.play(s);
+	}
+	catch(const SDLException e)
+	{
+		std::cout << "Could not play music: " << e.what() << "\n";
+		return EXIT_FAILURE;
+	}
+	
+	// run the windows event loop.
+	win.eventLoop();
+	return EXIT_SUCCESS;
+}
---
+++ examples/poly/poly.cpp
@@ -0,0 +1,95 @@
+/****************************************
+ *
+ * poly.cpp
+ * Define a epicpcm visualiser plugin.
+ *
+ * This file is part of mattulizer.
+ *
+ * Copyright 2011 (c) Matthew Leach.
+ *
+ * Mattulizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattulizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mattulizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "poly.h"
+#include "../../src/dspmanager.h"
+#include <SDL_opengl.h>
+#include <math.h>
+
+
+poly::poly(visualiserWin* win, int no_vertices, double step) : visualiser(win), threshold(3500000)
+{
+	// this plug-in needs the FFT DSP, set that up here.
+	FFT* fftPlugin = new FFT();
+	this->fftPlugin = fftPlugin;
+	win->getDSPManager()->registerDSPPlugin(fftPlugin);
+	this->no_vertices = no_vertices;
+	this->step = step;
+
+	vec_x = (double*)calloc(no_vertices, sizeof(double));
+	vec_y = (double*)calloc(no_vertices, sizeof(double));
+	vec_dir = (double*)calloc(no_vertices, sizeof(double));
+	for(int i = 0; i < no_vertices; i++)
+	{
+		vec_x[i] = (getRand() * 2) - 1;
+		vec_y[i] = (getRand() * 2) - 1;
+		vec_dir[i] = getRand() * 360;
+	}
+}
+
+double poly::getRand()
+{
+	return (double)rand() / RAND_MAX;
+}
+
+void poly::draw()
+{
+	// clear the screen.
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	// retrieve audio data.
+	FFTData* data = (FFTData*)fftPlugin->getDSPData();
+	if(data != NULL)
+	{
+		float total = 0;
+		for(int i = 0; i < 3; i++)
+		{
+			float complexArg = (float)sqrt(pow(data->data[i][0], 2) +
+			                               pow(data->data[i][1], 2));
+			total += complexArg;
+		}
+		total /= 3;
+		if(total > threshold)
+		{
+			for(int i = 0; i < no_vertices; i++)
+				vec_dir[i] = getRand() * 360;
+		}
+	}
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glBegin(GL_LINE_LOOP);
+	for(int i = 0; i < no_vertices; i++)
+	{
+		// Calculate the new position of the vertex.
+		vec_x[i] = (step * cos(vec_dir[i]) + vec_x[i]);
+		vec_y[i] = (step * sin(vec_dir[i]) + vec_y[i]);
+		if(vec_x[i] <= -1 || vec_x[i] >= 1)
+			vec_dir[i] = getRand() * 360;
+		if(vec_y[i] < -1 || vec_y[i] >= 1)
+			vec_dir[i] = getRand() * 360;
+		glVertex3f(vec_x[i], vec_y[i], 0.0f);
+	}
+	glEnd();
+	
+	// release the DSP data.
+	fftPlugin->relenquishDSPData();
+}
